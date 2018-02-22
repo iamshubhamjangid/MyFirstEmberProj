146423ec061ff22389b597395b1eacac6adebbfe
iamshubhamjangid <35128560+iamshubhamjangid@users.noreply.github.com>
2018-02-22 19:06:08 +0530

    Add files via upload

--- cdmgr.py
+++ cdmgr.py
@@ -1,28 +1,21 @@
-class candidatemgr:
+from Candidate import Candidate
+class candidatemgr():
     cddict={}
-    def __init__(self,name,aadhar,cid,age):
-	self.c_name=name
-	self.c_age=age
-	self.c_id=cid
-	self.c_aadhar=aadhar
-
-    def isValid(self):
-	if self.c_age>=28:
-            if candidatemgr.cddict.has_key(self.c_id):
+    def isValid(self,candidate):
+	if candidate.c_age>=28:
+            if self.cddict.has_key(candidate.c_id):
 	       print "User already exists!!!\n Not allowed to store multiple Same!!"
 	    return True
 	else:
 	    print("You are not satisfying the minimum requirements to be a candidtae!!!\n You are under 28!!")
-    @staticmethod
-    def addcandidate(self,name,cid):
-	candidatemgr.cddict[cid]=name
-	return candidatemgr.cddict
-    @staticmethod
-    def deletecandidate(self,cid):
-	del candidatemgr.cddict[cid]
-	return candidatemgr.cddict
-    @staticmethod
-    def displaycandidate(self):
-	print candidatemgr.cddict
+   
+    def add(self,candidate):
+	self.cddict[candidate.c_id]=candidate.c_name
+	
+    def delete(self,candidate):
+	del cddict[candidate.c_id]
+
+    def lists(self):
+	print self.cddict
 
     
--- elections.py
+++ elections.py
@@ -1,13 +1,13 @@
 import datetime
 from Voter import Voter
-from filemgr import filemanager
 from datetime import date
 from vtrmgr import votermgr
-from electionmgr import elmgr
+from electmgr import electionmgr
 from cdmgr import candidatemgr
+from Candidate import Candidate
+from filemgr import filemanager
 today = date.today()
 def main():
-  fm=filemanager()
   time=datetime.datetime.now()
   print("\n---------Welcome To Online Voting Portal---------\n")
   if time.hour<=18 and time.hour>=10:
@@ -21,13 +21,16 @@ def main():
                   age=int(input("Enter your age here\t"))
                   vid=raw_input("Enter your unique voter id here\t")
                   aadhar=raw_input("Enter your aadhar number here\t")          
-                  
-		  VoterMgr=votermgr(vid,aadhar,name,age)
-                  if VoterMgr.isValid(VoterMgr)==True :
-	            VoterMgr.add(VoterMgr)
-		    VoterMgr.listvoter()
+		  voter=Voter(name,aadhar,vid,age)          
+                  fm=filemanager()
+                  vtr=votermgr()
+                  if vtr.isValid(voter)==True :
+	             opt=raw_input("\n--------------\nChoose one the options\nAdd Voter---->(A)\nDelete Voter---->(D)\n")
+		     filemanager.Voteroptions[opt](voter) 
 		  vtch = raw_input("Add Voter?? [Y/N] \t")
 	      else:
+		  opt=raw_input("\n-------------\nChoose one the options\nDisplay Voter---->(V)\n")
+		  filemanager.listing[opt]()
 		  user = raw_input("Who are you?? Choose one of these:\nCandidate-->[C]\nVoter-->[V]\nExit--->[E]")
     	  elif user=='c' or user=='C':
 	      cdch = raw_input("Add Candidate?? [Y/N] \t")
@@ -35,19 +38,37 @@ def main():
     	          name=raw_input("Enter your name here\t")
                   age=int(input("Enter your age here\t"))
                   cid=raw_input("Enter your unique voter id here\t")
-                  aadhar=raw_input("Enter your aadhar number here\t")          
-                  cdt=candidatemgr(name,aadhar,cid,age)
-                  if cdt.isValid()==True :
-	              fm.addcandidate(name,cid)
-		      fm.displaycandidates()
+                  aadhar=raw_input("Enter your aadhar number here\t")
+		  candidate=Candidate(name,aadhar,cid,age)          
+                  fm=filemanager()   
+                  CdMgr=candidatemgr()
+                  if CdMgr.isValid(candidate)==True :
+		     opt=raw_input("Choose one the options\nAdd Candidate---->(A)\n Delete Candidate---->(D)\n")
+		     filemanager.Candidateoptions[opt](candidate)
 		  cdch = raw_input("Add Candidate?? [Y/N] \t")
 	      else:
+		  opt=raw_input("Choose one the options\nDisplay Candidate---->(C)\n")
+		  filemanager.listing[opt]()
 		  user = raw_input("Who are you?? Choose one of these:\nCandidate-->[C]\nVoter-->[V]\Exit--->[E]n")
 	  else:
-		elm=elmgr()
-		elecdate=elm.electiondate()
-		if today==elecdate:
+		print("\n------------------------------\nNow Election Manger will Commence the Election Date here\n-----------------------\n"
+		elm=electionmgr()
+		electiondate=elm.elecdate()
+		if today==electiondate:
 		    print("Now its voting time")
+		    while (time.hour<=17 and time.hour>=10):
+			vtr_id=raw_input("Enter your unique voter Id\t")
+			if votermgr.voterdict.has_key(vtr_id):
+				print("Now you have the following list of candidates for Voting\n Be careful while Voting as once you done with voting cannot be redone!!")
+				cdm=candidatemgr()
+				cdict=candidatemgr.cddict
+				for cndt in cdict:	
+					print cndt
+					print ""
+
+			else:
+				print("you are not registerd for voting!!!")			
+
 		else:
 		    print today
 		    print("Ye kya nautanki h!!")
